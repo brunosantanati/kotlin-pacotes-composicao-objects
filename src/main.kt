a6beebb1240edc264fa16c2705d7d7ad8a39f118
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-20 19:59:53 -0300

    Usar alias no import.

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,6 @@
+import java.lang.String as StringJava
+
 fun main() {
     val palavra: kotlin.String = ""
-    val palavraJava: java.lang.String = java.lang.String("")
+    val palavraJava: StringJava = StringJava("")
 }
