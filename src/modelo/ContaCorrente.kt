92582c4b1b059edd46d09229b166833560119e21
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-20 12:13:31 -0300

    Organizar arquivos em pacotes.

---
+++ src/modelo/ContaCorrente.kt
@@ -0,0 +1,16 @@
+package modelo
+
+class ContaCorrente(
+    titular: String,
+    numero: Int
+) : Conta(
+    titular = titular,
+    numero = numero
+) {
+    override fun saca(valor: Double) {
+        val valorComTaxa = valor + 0.1
+        if(this.saldo >= valorComTaxa){
+            this.saldo -= valorComTaxa
+        }
+    }
+}
--- src/modelo/FuncionarioAdmin.kt
+++ src/modelo/FuncionarioAdmin.kt
@@ -1,3 +1,5 @@
+package modelo
+
 abstract class FuncionarioAdmin(
     nome: String,
     cpf: String,
