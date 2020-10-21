812c59981425c641ab8740d557725f14e99de933
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-21 16:18:03 -0300

    Testando metodos herdados de Any(equals, hashCode e toString)

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,7 +1,22 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.teste.testaAutenticacaoTiposDiferentes
+import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
-    testaAutenticacaoTiposDiferentes()
+    val endereco = Endereco()
+    val enderecoNovo = Endereco()
+    val enderecoNovo2 = endereco
+
+    println(endereco.equals(endereco)) //equals que Endereco herda de Any
+    println(endereco.equals(enderecoNovo))
+    println(endereco.equals(enderecoNovo2))
+
+    println(endereco.hashCode()) //hashCode que Endereco herda de Any
+    println(enderecoNovo.hashCode())
+
+    println(endereco.toString()) //toString que Endereco herda de Any
+    println(enderecoNovo.toString())
+
+    //simulando um toString() padrão:
+    println("${endereco.javaClass}@${java.lang.Integer.toHexString(endereco.hashCode())}")
 }
