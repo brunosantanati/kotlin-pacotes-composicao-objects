b15da0036c97bf0b0dc1e11054f6fa3cec3fe2ba
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-21 13:44:49 -0300

    Contabilizando contas criadas do jeito errado.

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,7 +1,16 @@
 package br.com.alura.bytebank
 
+import br.com.alura.bytebank.modelo.Cliente
+import br.com.alura.bytebank.modelo.ContaPoupanca
 import br.com.alura.bytebank.teste.testaContasDiferentes
 
 fun main() {
+    var totalContas = 0
+    val alex = Cliente(nome = "Alex", cpf = "", senha = 1)
+    ContaPoupanca(titular = alex, numero = 1000)
+    totalContas++
+
     testaContasDiferentes()
+
+    println("Total de contas: $totalContas")
 }
