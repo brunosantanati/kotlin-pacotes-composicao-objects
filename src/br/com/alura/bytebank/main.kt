b7a327cb9948b292307a654290dcdb50db4baa6b
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-21 15:21:48 -0300

    Entendendo a classe Any.

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,7 +1,37 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.teste.testaObjects
+import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
-    testaObjects()
+
+    val endereco = Endereco()
+
+    println()
+    println(1)
+    println(1.0)
+    println(true)
+    println("teste")
+    println(endereco) //isso funciona porque o Kotlin implementou a função recebendo um objeto do tipo Any
+
+    //sobregarca
+    imprime()
+    imprime(1)
+    imprime(1.0)
+    imprime(endereco) //criamos uma função que recebe um Endereco
+}
+
+fun imprime(){
+
+}
+
+fun imprime(valor: Int){
+
+}
+
+fun imprime(valor: Double){
+
+}
+
+fun imprime(endereco: Endereco){
+
 }
